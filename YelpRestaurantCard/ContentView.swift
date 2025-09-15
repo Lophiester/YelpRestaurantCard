cffadc8f9eb150e4905f0b4c1109397b9fd8a8a3
Charles <dbsycg@gmail.com>
2025-09-15 22:53:47 +0900

    Initial Commit

---
+++ YelpRestaurantCard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YelpRestaurantCard
+//
+//  Created by Charles Yamamoto on 2025/09/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YelpRestaurantCard/YelpRestaurantCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  YelpRestaurantCardApp.swift
+//  YelpRestaurantCard
+//
+//  Created by Charles Yamamoto on 2025/09/15.
+//
+
+import SwiftUI
+
+@main
+struct YelpRestaurantCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
